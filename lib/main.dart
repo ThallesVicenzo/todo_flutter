9b93c626114722183c9a13ae6347de832515ccf1
ThallesVicenzo <thalles.vicenzo.costa@gmail.com>
2022-05-16 16:06:55 -0300

    versions adapted and imports duplicated fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_flutter/Screens/TaskScreen.dart';
-import 'package:provider/provider.dart';
 import 'package:todo_flutter/models/task_data.dart';
 
 void main() {
